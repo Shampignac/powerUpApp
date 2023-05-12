6b5c01f441b976c64a2ac0fe47b46b068e7460c2
Jérome Marchand <marchandjerome@cedeg.be>
2023-05-12 13:47:55 +0200

    delete caroussel

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,9 @@
 <template>
   <main class="main">
-    <Carousel>
-      <Slide v-for="image in images" :key="image.id">
-        <div class="carousel__item">
-          <img :src="image.src" :alt="image.alt" />
-        </div>
-      </Slide>
-
-      <template #addons>
-        <Navigation />
-        <Pagination />
-      </template>
-    </Carousel>
+    
     <div class="main__container">
       <h2 class="main__container__subtitle subtitle">Dernier épisode</h2>
-      <h1 class="main__container__title title">Katan VS tronçonneuse</h1>
+      <h1 class="main__container__title title">Katana VS tronçonneuse</h1>
       <h3 class="main__container__subsubtitle subsubtitle">
         Chapitres 35-36-37-38-40
       </h3>
@@ -28,20 +17,7 @@
 </template>
 
 <style scoped lang="scss">
-.carousel__item {
-  border-radius: 8px;
-  padding: none;
-}
 
-.carousel__slide {
-  padding: 10px;
-}
-
-.carousel__prev,
-.carousel__next {
-  box-sizing: content-box;
-  border: 5px solid white;
-}
 .main {
   max-width: 100%;
 
@@ -58,6 +34,7 @@
     }
     &__title {
       grid-area: 2 / 1 / 3 / 2;
+      font-size: xx-large;
     }
     &__img {
       grid-area: 3 / 1 / 4 / 2;
@@ -71,8 +48,7 @@
     }
   }
 }
-
-@media screen and (max-width: 800px) {
+@media screen and (max-width: 767px) {
   .main {
     &__container {
       margin: 0 1em;
@@ -99,44 +75,8 @@
     }
   }
 }
-
 </style>
 
 <script>
-import { defineComponent } from "vue";
-import { Carousel, Navigation, Pagination, Slide } from "vue3-carousel";
 
-import "vue3-carousel/dist/carousel.css";
-
-export default defineComponent({
-  name: "Basic",
-  components: {
-    Carousel,
-    Slide,
-    Pagination,
-    Navigation,
-  },
-  data() {
-    return {
-      images: [
-        {
-          id: 1,
-          src: "./medias/chainsaw-man-episode-7.png",
-          alt: "Power personnage",
-        },
-        {
-          id: 2,
-          src: "./medias/Chainsaw-Man-Saison-1-episode-1-1600x900.jpg",
-          alt: "Image 2 alt text here",
-        },
-        {
-          id: 3,
-          src: "./medias/Chainsaw-Man-episode-12-1600x900.jpg",
-          alt: "Image 3 alt text here",
-        },
-      ],
-    };
-  },
-});
-
-</script>
+;</script>
